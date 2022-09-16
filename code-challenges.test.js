3e0c2bc20d79fe262110b8337d6104989859ecfd
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-09-16 16:00:35 -0700

    making progress

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+const { array } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -25,6 +27,15 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("divisibleByThree", () => {
+    it("takes a object as an argument and decides if the number inside it is evenly divisible by three or not.", () => {
+        expect(divisibleByThree(object1)).toEqual("15 is divisible by three")
+        expect(divisibleByThree(object2)).toEqual("0 is divisible by three")
+        expect(divisibleByThree(object3)).toEqual("-7 is not divisible by three")
+    })
+})
+// ReferenceError: divisibleByThree is not defined
+
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
 const object2 = { number: 0 }
@@ -34,9 +45,33 @@ const object3 = { number: -7 }
 
 // b) Create the function that makes the test pass.
 
+// -------- process --------
+// input: object.number
+// output: return if object number is divisible by three
+// process: create an if statement 
+
+let divisibleByThree = (object) => {
+  if (object.number % 3 === 0) {
+    return `${object.number} is divisible by three`
+  } else if (object.number % 3 !== 0) {
+    return `${object.number} is not divisible by three`
+  } else {
+    return "error"
+  }
+ 
+} 
+
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("capitalWords", () => {
+    it("returns an array with all the words capitalized.", () => {
+        expect(capitalWords(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(capitalWords(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+    })
+})
+// ReferenceError: capitalWords is not defined
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -45,9 +80,24 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 
 // b) Create the function that makes the test pass.
 
+const capitalWords = (nouns) => {
+    return nouns.map((nouns) => {
+        const lowercase = nouns.toLowerCase()
+        return nouns.charAt(0).toUpperCase() + lowercase.slice(1)
+    })
+}
+
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("indexVowel", () => {
+    it("returns an array with all the words capitalized.", () => {
+        expect(indexVowel(vowelTester1)).toEqual(1)
+        expect(indexVowel(vowelTester2)).toEqual(0)
+        expect(indexVowel(vowelTester3)).toEqual(2)
+    })
+})
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -56,4 +106,10 @@ const vowelTester2 = "academy"
 const vowelTester3 = "challenges"
 // Expected output: 2
 
-// b) Create the function that makes the test pass.
+// ReferenceError: indexVowel is not defined
+
+// b) Create the function that makes the test pass
+
+const indexVowel = (vowels) => {
+    return indexOf.(vowelT)
+}
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,17 +8,26 @@
 // --------------------1) What will this log?
 
 const cohort = "Foxtrot 2022"
-// console.log(cohort.split(""))
+//console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Using .split should split the string "Foxtrot 2022" into an array, seperating every character like ["F", "o", "x", "t" etc..]
+// b) Verify and explain: It did split into a seperated array like I assumed, however I got the syntax wrong, it should be single apostrophes instead of quotes around each character, like so: ['F', 'o', 'x', 't' etc..]
 
 // --------------------2) What will this log?
 
-const greeter = (name) => {
-  `Hello, ${name}!`
-}
+// const greeter = (name) => {
+//   `Hello, ${name}!`
+// }
 // console.log(greeter("LEARN Student"))
+
+
+// const greeter = (name) => {
+//   `Hello, ${name}!`
+// }
+// console.log(greeter("LEARN Student"))
+
+
+
 
 // a) Your answer:
 // b) Verify and explain:
@@ -26,17 +35,17 @@ const greeter = (name) => {
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
-// console.log(multipliedByTwo)
+//console.log(multipliedByTwo)
 
-// a) Your answer:
+// a) Your answer: It should multiply the numbers in the array by 2
 // b) Verify and explain:
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+//console.log(onlyOdds)
 
-// a) Your answer:
+// a) Your answer: It should result in: [11, 13, 15]. It's filtering the odd numbers to be the result by using .filter, and then using number % 2 !== 0 to determine if the numbers are odd.
 // b) Verify and explain:
 
 // --------------------5) What will this log?
@@ -47,9 +56,9 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+//console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
+// a) Your answer: This will log JavaScript, as it's asking for the string inside of languages, and it's asking for the index[0] of languages which would be the first string: JavaScript
 // b) Verify and explain:
 
 // --------------------STRETCH: What will this log?
@@ -62,7 +71,7 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
+// a) Your answer: George, Foxtrot 2022
 // b) Verify and explain:
