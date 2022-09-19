5ddabdcba5c81aed9f89a5352f306e8c53ad37de
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-09-18 21:50:59 -0700

    finished 2/3

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,6 +1,6 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
-const { array } = require("yargs")
+
 
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
@@ -26,6 +26,7 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
+// First I'll make my jest test using divisibleByThree as the name of my function. And then using divisibleByThree on each object to see if it will equal the desired output of number is divisible or not divisible by three.
 
 describe("divisibleByThree", () => {
     it("takes a object as an argument and decides if the number inside it is evenly divisible by three or not.", () => {
@@ -35,6 +36,7 @@ describe("divisibleByThree", () => {
     })
 })
 // ReferenceError: divisibleByThree is not defined
+// good error!
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -45,12 +47,12 @@ const object3 = { number: -7 }
 
 // b) Create the function that makes the test pass.
 
-// -------- process --------
-// input: object.number
-// output: return if object number is divisible by three
-// process: create an if statement 
+// First I will create a function called divisibleByThree with the parameter of (object)
+// Then I will create a conditional statement that takes the (object) and if divisble by three using (% 3 === 0) then return using string interpolation that the ${object.number} is divisble by three.
+// Then I will create an else if for the opposite, if it's not divisble by three using (% 3 !== 0) then it will return another string interpolation that the ${object.number} is NOT divisible by three.
+// Finally a catch all else statement to return an "error" for any other outcome.
 
-let divisibleByThree = (object) => {
+const divisibleByThree = (object) => {
   if (object.number % 3 === 0) {
     return `${object.number} is divisible by three`
   } else if (object.number % 3 !== 0) {
@@ -64,6 +66,7 @@ let divisibleByThree = (object) => {
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
+// First I'll create my jest test using capitalWords as the name of my function. Then using the capitalWords function on randomNouns1 & 2, toEqual the desired capitlized arrays.
 
 describe("capitalWords", () => {
     it("returns an array with all the words capitalized.", () => {
@@ -72,6 +75,7 @@ describe("capitalWords", () => {
     })
 })
 // ReferenceError: capitalWords is not defined
+// Good error
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -79,17 +83,23 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
 
 // b) Create the function that makes the test pass.
+// First I will create a function called capitalWords
+// Then I will return a new array using.map
+// Then using charAt(0) to target the first index of each string, the first letter that we need to capitalize.
+// To then capitalize the index 0 of that string we'd use .toUpperCase()
+// Then we'd use slice(1) and .toLowerCase() to ensure that the rest of the strings in the array are not capitalized like the first letters are.
 
-const capitalWords = (nouns) => {
-    return nouns.map((nouns) => {
-        const lowercase = nouns.toLowerCase()
-        return nouns.charAt(0).toUpperCase() + lowercase.slice(1)
+
+const capitalWords = (array) => {
+    return array.map((value) => {
+        return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
     })
 }
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
+// First I'll create my test using indexVowel as the function name, and using indexVowel on each vowelTester, we want them toEqual the number of the index in which the first vowel is in each string.
 
 describe("indexVowel", () => {
     it("returns an array with all the words capitalized.", () => {
@@ -107,9 +117,24 @@ const vowelTester3 = "challenges"
 // Expected output: 2
 
 // ReferenceError: indexVowel is not defined
+// Good error
 
 // b) Create the function that makes the test pass
+// First I created a function called indexVowel
+// Then we'll create a for loop with the variable i = 0, and define the condition i must be less than the length of the string with <str.length, and finally i++ which will increase a value each time the loop has been executed.
+// then a conditional if statement that if the string[i] has the a,e,i,o, or u vowels (seperated by || OR) then it will return i.
 
-const indexVowel = (vowels) => {
-    return indexOf.(vowelT)
-}
+
+const indexVowel = (str) => {
+    for (let i = 0; i <str.length; i++) {
+        if (
+            str[i] === "a" || 
+            str[i] === "e" ||
+            str[i] === "i" ||
+            str[i] === "o" ||
+            str[i] === "u") {
+            return i
+          }
+        }
+      }
+
--- coding-concepts.js
+++ coding-concepts.js
@@ -15,38 +15,29 @@ const cohort = "Foxtrot 2022"
 
 // --------------------2) What will this log?
 
-// const greeter = (name) => {
-//   `Hello, ${name}!`
-// }
+const greeter = (name) => {
+  `Hello, ${name}!`
+}
 // console.log(greeter("LEARN Student"))
 
-
-// const greeter = (name) => {
-//   `Hello, ${name}!`
-// }
-// console.log(greeter("LEARN Student"))
-
-
-
-
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The console log should return the string "Hello, LEARN Student
+// b) Verify and explain: It instead returned 'undefined'. It took me a good while to realize what was missing in this code, until I realized it was in the answer that I gave, it returned undefined because there was no 'return' in the function. All we need is a return right before the string interpolation of `Hello, ${name}!`. Once that is added we'll get our desired console.log of "Hello, LEARN student"
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
-//console.log(multipliedByTwo)
+// console.log(multipliedByTwo)
 
-// a) Your answer: It should multiply the numbers in the array by 2
-// b) Verify and explain:
+// a) Your answer: It should make a new array with the previous numbers inside multiplied by 2.
+// b) Verify and explain: It did return [ 8, 10, 12, 14, 16] each number multiplied by 2 in a new array using the .map function and then using number * 2 to multiply by 2.
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-//console.log(onlyOdds)
+// console.log(onlyOdds)
 
-// a) Your answer: It should result in: [11, 13, 15]. It's filtering the odd numbers to be the result by using .filter, and then using number % 2 !== 0 to determine if the numbers are odd.
-// b) Verify and explain:
+// a) Your answer: It should just bring in the odd numbers 11,13,15.
+// b) Verify and explain: It did create a new array of [11,13,15]. It's filtering the odd numbers in the new array to be the result by using .filter, and then using number % 2 !== 0 to see if they are NOT divisible by 2 using !== 0 which would result in only the three odd numbers.
 
 // --------------------5) What will this log?
 
@@ -56,10 +47,10 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-//console.log(myCodingSkills.languages[0])
+// console.log(myCodingSkills.languages[0])
 
-// a) Your answer: This will log JavaScript, as it's asking for the string inside of languages, and it's asking for the index[0] of languages which would be the first string: JavaScript
-// b) Verify and explain:
+// a) Your answer: This should just log JavaScript
+// b) Verify and explain: This logged JavaScript, as it's asking for the string inside of languages array, and it's asking for the index[0] of languages which would be the first string: JavaScript
 
 // --------------------STRETCH: What will this log?
 
@@ -73,5 +64,5 @@ class Learn {
 const learnStudent = new Learn("George")
 console.log(learnStudent)
 
-// a) Your answer: George, Foxtrot 2022
-// b) Verify and explain:
+// a) Your answer: This should log George, Foxtrot 2022
+// b) Verify and explain: It logged: Learn { student: 'George', cohort: 'Foxtrot', year: 2022 } While it did pull George, Foxtrot, and 2022 like I thought, it returned it as class object having the this.variableNames (student, cohort, and year) show up as that's what the constructor was meant to display.
